76f5fb96274f88f2f6b97efa0bb98ea0971a9542
Alex Jesus <100452419+xAJesus@users.noreply.github.com>
2022-02-27 23:31:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.getElementById('gradient');
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+
+function setGradient() {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+
+    css.textContent = body.style.background + ';';
+}
+
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
